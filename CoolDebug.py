ae7b5a2287c4520fb472e9d46ddddd66882d5bb1
sam <isfwic1@gmail.com>
2024-02-08 13:18:07 +0000

    version 0.1 - add AIP

--- CoolDebug.py
+++ CoolDebug.py
@@ -16,19 +16,19 @@ exec(urlopen('https://raw.githubusercontent.com/persamplex/online-tools/main/AIP
 try:
     from colorama import Fore, Style
 except ModuleNotFoundError:
-    AIP.install('colorama')
+    AIP().install('colorama')
     from colorama import Fore, Style
 
 try:
     from pytz import timezone
 except ModuleNotFoundError :
-    AIP.install('pytz')
+    AIP().install('pytz')
     from pytz import timezone
 
 try:   
     from persiantools.jdatetime import JalaliDate
 except ModuleNotFoundError :
-    AIP.install('persiantools')
+    AIP().install('persiantools')
     from persiantools.jdatetime import JalaliDate
 
 
